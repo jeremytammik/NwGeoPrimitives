79cd0b6c927f120dcda33cc6226d9677e818f142
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-12-17 15:30:38 +0100

    implemented fragment dictionary

--- NwGeoPrimitives/GeoPrimitives.cs
+++ NwGeoPrimitives/GeoPrimitives.cs
@@ -1,16 +1,66 @@
 ﻿#region Namespaces
 using System;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using ComBridge = Autodesk.Navisworks.Api.ComApi.ComApiBridge;
-using COMApi = Autodesk.Navisworks.Api.Interop.ComApi;
+using ComApi = Autodesk.Navisworks.Api.Interop.ComApi;
 using NwVertex = Autodesk.Navisworks.Api.Interop.ComApi.InwSimpleVertex;
+using System.Collections.Generic;
+using Autodesk.Navisworks.Api;
 #endregion // Namespaces
 
 namespace NwGeoPrimitives
 {
+  static class Extension
+  {
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static T[] ToArray<T>( this Array arr )
+      where T : struct
+    {
+      T[] result = new T[ arr.Length ];
+      Array.Copy( arr, result, result.Length );
+      return result;
+    }
+  }
+
   public class GeoPrimitives
   {
-    class CallbackGeomListener : COMApi.InwSimplePrimitivesCB
+    #region GetSortedFragments
+    // from thread on Faster primitive data extraction:
+    // https://forums.autodesk.com/t5/navisworks-api/faster-primitive-data-extraction/td-p/9298425
+    Dictionary<int[], Stack<ComApi.InwOaFragment3>> 
+      GetSortedFragments( ModelItemCollection modelItems )
+    {
+      ComApi.InwOpState oState = ComBridge.State;
+      ComApi.InwOpSelection oSel = ComBridge.ToInwOpSelection( modelItems );
+
+      // To be most efficient, you need to lookup an efficient 
+      // EqualityComparer for the int[] key
+
+      var pathDict = new Dictionary<int[], Stack<ComApi.InwOaFragment3>>();
+
+      foreach( ComApi.InwOaPath3 path in oSel.Paths() )
+      {
+        // This yields ONLY unique fragments
+        // ordered by geometry they belong to
+
+        foreach( ComApi.InwOaFragment3 frag in path.Fragments() )
+        {
+          int[] pathArr = ((Array) frag.path.ArrayData).ToArray<int>();
+          if( !pathDict.TryGetValue( pathArr, 
+            out Stack<ComApi.InwOaFragment3> frags ) )
+          {
+            frags = new Stack<ComApi.InwOaFragment3>();
+            pathDict[ pathArr ] = frags;
+          }
+          frags.Push( frag );
+        }
+      }
+      return pathDict;
+    }
+    #endregion // GetSortedFragments
+
+    class CallbackGeomListener : ComApi.InwSimplePrimitivesCB
     {
       static public long LineCount { get; set; }
       static public long PointCount { get; set; }
@@ -50,17 +100,17 @@ namespace NwGeoPrimitives
     }
 
     void walkNode(
-      COMApi.InwOaNode parentNode,
+      ComApi.InwOaNode parentNode,
       bool bFoundFirst )
     {
       if( parentNode.IsGroup )
       {
-        COMApi.InwOaGroup group = (COMApi.InwOaGroup) parentNode;
+        ComApi.InwOaGroup group = (ComApi.InwOaGroup) parentNode;
         long n = group.Children().Count;
 
         for( long i = 1; i <= n; ++i )
         {
-          COMApi.InwOaNode newNode = group.Children()[i];
+          ComApi.InwOaNode newNode = group.Children()[i];
 
           if( (!bFoundFirst) && (n > 1) )
           {
@@ -71,25 +121,45 @@ namespace NwGeoPrimitives
       }
       else if( parentNode.IsGeometry )
       {
-        CallbackGeomListener cbl 
-          = new CallbackGeomListener();
-
-        COMApi.InwNodeFragsColl fragsColl 
+        ComApi.InwNodeFragsColl fragsColl
           = parentNode.Fragments();
+
+        CallbackGeomListener cbl 
+          = generate_primitives_for_all_fragments
+            ? new CallbackGeomListener()
+            : null;
 
         long nFrags = fragsColl.Count;
 
-        Debug.WriteLine( "frags count:" 
+        Debug.WriteLine( "frags count:"
           + nFrags.ToString() );
 
         for( long i = 1; i <= nFrags; ++i )
         {
-          COMApi.InwOaFragment3 frag = fragsColl[i];
+          ComApi.InwOaFragment3 frag = fragsColl[ i ];
 
-          frag.GenerateSimplePrimitives(
-            COMApi.nwEVertexProperty.eNORMAL, cbl );
+          if( generate_primitives_for_all_fragments )
+          {
+            frag.GenerateSimplePrimitives(
+              ComApi.nwEVertexProperty.eNORMAL, cbl );
+          }
+          else
+          {
+            // Collect single instance of each fragment
+            // in lookup dictionary for later call to
+            // GenerateSimplePrimitives
+
+            int[] pathArr = ((Array) frag.path.ArrayData).ToArray<int>();
+            if( !_pathDict.TryGetValue( pathArr,
+              out Stack<ComApi.InwOaFragment3> frags ) )
+            {
+              frags = new Stack<ComApi.InwOaFragment3>();
+              _pathDict[ pathArr ] = frags;
+            }
+            frags.Push( frag );
+          }
+          _nFragsTotal += nFrags;
         }
-        _nFragsTotal += nFrags;
         ++_nNodesTotal;
       }
     }
@@ -97,18 +167,50 @@ namespace NwGeoPrimitives
     long _nNodesTotal;
     long _nFragsTotal;
 
+    /// <summary>
+    /// If true, naively call GenerateSimplePrimitives 
+    /// on every fragment encountered by `walkNode`; 
+    /// if false, collect them in a dictionary instead,
+    /// and generate primitives once only for each fragment.
+    /// </summary>
+    bool generate_primitives_for_all_fragments = false;
+
+    /// <summary>
+    /// Dictionary mapping fragment paths to fragments
+    /// used to collect only one fragment instance for each path
+    /// </summary>
+    Dictionary<int[], Stack<ComApi.InwOaFragment3>> _pathDict;
+
     public void Execute()
     {
       _nNodesTotal = 0;
       _nFragsTotal = 0;
-      CallbackGeomListener.Init();
+      _pathDict = new Dictionary<int[], Stack<ComApi.InwOaFragment3>>();
 
       DateTime dt = DateTime.Now;
 
+      CallbackGeomListener.Init();
+
       // Convert to COM selection
 
-      COMApi.InwOpState oState = ComBridge.State;
+      ComApi.InwOpState oState = ComBridge.State;
       walkNode( oState.CurrentPartition, false );
+
+      // Generate primitives for collected fragments
+
+      CallbackGeomListener cbl
+        = new CallbackGeomListener();
+
+      foreach( var kvp in _pathDict )
+      {
+        var frags = kvp.Value;
+        while( frags.Count > 0 )
+        {
+          ComApi.InwOaFragment3 frag = frags.Pop();
+          frag.GenerateSimplePrimitives(
+            ComApi.nwEVertexProperty.eNORMAL, cbl );
+        }
+      }
 
       TimeSpan ts = DateTime.Now - dt;
       double ms = ts.TotalMilliseconds;
--- NwGeoPrimitives/Main.cs
+++ NwGeoPrimitives/Main.cs
@@ -1,5 +1,8 @@
 ﻿#region Namespaces
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.DocumentParts;
 using Autodesk.Navisworks.Api.Plugins;
+using System.Diagnostics;
 #endregion // Namespaces
 
 namespace NwGeoPrimitives
@@ -11,6 +14,17 @@ namespace NwGeoPrimitives
   {
     public override int Execute( params string[] ps )
     {
+      Document doc = Application.ActiveDocument;
+      Debug.Print( "{0}:{1}", doc.CurrentFileName + doc.Title );
+
+      string filename = doc.FileName;
+      Units units = doc.Units;
+      DocumentModels models = doc.Models;
+      DocumentInfoPart info = doc.DocumentInfo;
+      DocumentDatabase db = doc.Database;
+      bool ignoreHidden = true;
+      BoundingBox3D bb = doc.GetBoundingBox( ignoreHidden );
+
       GeoPrimitives gp = new GeoPrimitives();
       gp.Execute();
       return 0;
